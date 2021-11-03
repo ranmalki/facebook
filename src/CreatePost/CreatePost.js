4fc63d00030ef8e6a600e389beb2ff89f3321bd8
Tamir Segal <tamseg56@gmail.com>
2021-11-03 19:49:04 +0200

    Tamir-1st create post

--- src/CreatePost/CreatePost.js
+++ src/CreatePost/CreatePost.js
@@ -3,8 +3,14 @@ import './CreatePost.css';
 
 function CreatePost() {
     return (
-        <div>
-            
+        <div className="create-post-wrapper">
+            <div className="create-post">
+                <h1 className="create-post-header">Create post</h1>
+                <input className="post-input" placeholder="What's on your mind?" />
+                <div className="create-post-btn-wrapper">
+                    <button className="create-post-btn">Submit</button>
+                </div>
+            </div>
         </div>
     );
 }
